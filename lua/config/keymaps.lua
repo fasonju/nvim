e1bce22cf2841d834047bcd75835dc493ebc3e27
Jason Fu <r.fu1@student.tue.nl>
2025-03-29 17:54:57 +0100

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- avoid paragraph jumps from opening folds
+vim.keymap.set("n", "{", "{", { noremap = true, silent = true })
+vim.keymap.set("n", "}", "}", { noremap = true, silent = true })
